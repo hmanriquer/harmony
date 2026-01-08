ade771ee2779ba82f60799f887282e274ccf0c68
Hamed Manrique <manrique.hamed1610@gmail.com>
2026-01-07 21:20:35 -0600

    fix: added icon and added some qol updates

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -1,11 +1,28 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
 import { Moon, Sun } from 'lucide-react';
 import { Button } from './ui/button';
 
 export function ToggleTheme() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMounted(true);
+    }, 0);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <Button variant="ghost">
+        <Sun className="invisible" />
+      </Button>
+    );
+  }
 
   return (
     <Button
